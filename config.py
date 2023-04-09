7ed3d0697b46ea73de69d895202715812397ea79
Sagar Tiwari <112709751+stkeditz@users.noreply.github.com>
2023-04-09 10:14:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID","6117249994"))
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
-SESSION = getenv("SESSION","BQB2kRkEUE7cp0YE5m-270uQeyYPg9-DMFbvpkE-0q-x4C35hUjIEiPOap1H60TOHgSSwJPQbAGsiI2Zt8eSaxBe25v8aVkT42GQrtTs6BstPlcPHuMOuExM8irlnuXI-CTpIcplVBYtIhwO4uHKPaXWUMPYMllwzpB3G41V9hvCHDnV5m3ZFzD1zsik5GbGowQDDZtEhy2hUxY92Dm40OpsVqY5TKKKXIVYFIaaiTQ5ToTOpOVri1IbNUbhtA1gRHtlz9NpnG-JY7vmh6ELO1BaVcxPce0yokGWTrhlyyX-EH-acTk5fYdbC570fEVfTj-VuQfoH1_PfB_YcsRRaeAAAAAWp7l-4A")
+SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SankiDiscuss")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
